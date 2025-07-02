82e137d17dd9a8957e43ea08b69f71317a614a80
Sam Mauldin <sam@mauldin.me>
2025-07-02 11:30:50 -0700

    add homebrew to fish config

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,6 +1,11 @@
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
 
+if test -d /opt/homebrew
+    /opt/homebrew/bin/brew shellenv | source
+end
+
 if status is-interactive
     zoxide init fish | source
 end
+
